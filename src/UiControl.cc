1754de288eef7ee3be0a231bba666df6cc174e90
Andrea Parodi <andrea@parro.it>
2016-08-05 15:34:16 +0200

    restored uicontrol

--- src/UiControl.cc
+++ src/UiControl.cc
@@ -2,61 +2,13 @@
 #include "nbind/api.h"
 #include "nbind/nbind.h"
 #include "ui-node.h"
-#include <map>
-#include <nan.h>
-
-std::unordered_map <uiControl *, Nan::Persistent<v8::Object> *> controlsMap;
-std::unordered_map <uiControl *, UiControl *> UiControlMap;
-
 
 uiControl * UiControl::getHandle() {
 	return handle;
 }
 
-static void onDestroy(uiControl *c) {
-	printf("destroy control %p\n", c);
-	UiControl * self = UiControlMap[c];
-	(*(self->originalDestroy))(c);
-	UiControlMap.erase(c);
-}
-
-static void onSetParent(uiControl *c, uiControl *p) {
-	printf("onSetParent %p=%p\n", c, p);
-	UiControl * self = UiControlMap[c];
-
-	if (p != NULL) {
-		Nan::Persistent<v8::Object> *pers = nbind::BindWrapper<UiControl>::findInstance(self);
-		Nan::Persistent<v8::Object> * other = new Nan::Persistent<v8::Object>(Nan::New(*pers));
-		printf("set persistent %p for %p\n", other, c);
-		controlsMap[c] = other;
-	} else {
-		Nan::Persistent<v8::Object> * persy = controlsMap[c];
-		printf("remove persy %p for %p\n", persy, c);
-		persy->Reset();
-		printf("reset done\n");
-		controlsMap.erase(c);
-		printf("erase done\n");
-	}
-
-	(*(self->originalSetParent))(c, p);
-	printf("originalSetParent done %p\n",self->originalSetParent);
-}
-
 UiControl::UiControl(uiControl* hnd) {
 	handle = hnd;
-	originalDestroy = hnd->Destroy;
-	originalSetParent = hnd->SetParent;
-	hnd->Destroy = onDestroy;
-	hnd->SetParent = onSetParent;
-
-	UiControlMap[hnd] = this;
-
-	printf("created %p\n", handle);
-}
-
-UiControl::~UiControl() {
-
-	printf("destroy c++ %p\n", handle);
 }
 
 void UiControl::destroy() {
